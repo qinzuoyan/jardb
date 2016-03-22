ba64b4951a8b6f07a69af04a30b810808c2539f1
qinzuoyan <qinzuoyan@xiaomi.com>
2016-03-22 11:32:59 +0800

    refactors

---
+++ src/main/java/dsn/operator/rrdb_merge_operator.java
@@ -0,0 +1,30 @@
+package dsn.operator;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TProtocol;
+
+import dsn.apps.rrdb;
+import dsn.apps.update_request;
+import dsn.replication.write_request_header;
+
+public class rrdb_merge_operator extends rrdb_write_operator {
+    public rrdb_merge_operator(write_request_header header, update_request request) {
+        super(header);
+        this.request = request;
+    }
+
+    public void client_send(rrdb.Client client) throws TException {
+        client.send_merge(header, request);
+    }
+
+    public void recv_data(TProtocol iprot) throws TException {
+        rrdb.merge_result result = new rrdb.merge_result();
+        result.read(iprot);
+        if (result.isSetSuccess())
+            resp = result.success;
+        else
+            throw new org.apache.thrift.TApplicationException(org.apache.thrift.TApplicationException.MISSING_RESULT, "merge failed: unknown result");
+    }
+
+    private update_request request;
+}
---
+++ src/main/java/dsn/operator/rrdb_operator.java
@@ -0,0 +1,25 @@
+package dsn.operator;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.TServiceClient;
+
+import dsn.apps.rrdb;
+import dsn.thrift.TMsgBlockProtocol;
+
+public abstract class rrdb_operator extends client_operator {
+    private void set_request_hash(rrdb.Client client, dsn.replication.global_partition_id gpid) {
+        ((TMsgBlockProtocol) client.getOutputProtocol()).setRequestHash(gpid.app_id ^ gpid.pidx);
+    }
+
+    public final void send(TServiceClient client) throws TException {
+        rrdb.Client c = (rrdb.Client) (client);
+        set_request_hash(c, get_op_gpid());
+        client_send(c);
+    }
+
+    public abstract void client_send(rrdb.Client client) throws TException;
+
+    public abstract dsn.base.error_code get_result_error();
+
+    public abstract dsn.replication.global_partition_id get_op_gpid();
+}
